6cd6f64f29cec115e9211a397e2312309f528c7e
JeongHyeongKim <maladroit1@likelion.org>
2020-05-19 09:24:36 +0900

    initial update

---
+++ src/main/java/com/jeonghyeong/TStoryDashBoardApplication.java
@@ -0,0 +1,13 @@
+package com.jeonghyeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TStoryDashBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TStoryDashBoardApplication.class, args);
+	}
+
+}
